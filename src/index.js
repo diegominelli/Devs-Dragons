a50f49b1d155d0407250f2f107796faceef41095
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2023-06-14 17:23:45 -0300

    criacao da classe personagem

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
-const personagemPedrinho = {
-    nome: 'Pedrinho',
-    vida: 7,
-    mana: 12,
-    level: 5,
-    tipo: 'Mago',
-}
+import { Personagem } from "./modules/personagem.js"
+
+// const personagemPedrinho = {
+//   nome: 'Pedrinho',
+//   vida: 7,
+//   mana: 12,
+//   level: 5,
+//   tipo: 'Mago',
+// }
 
 const personagemJose = {
-    nome: 'Jose',
-    vida: 7,
-    mana: 6,
-    level: 3,
-    tipo: 'Arqueiro',
+  nome: 'Jose',
+  vida: 7,
+  mana: 6,
+  level: 3,
+  tipo: 'Arqueiro',
 }
+
+const personagemAna = {
+  nome: 'Ana',
+  vida: 8,
+  mana: 10,
+  level: 3,
+  tipo: 'Arqueiro',
+}
+
+const personagemPedrinho = new Personagem()
+personagemPedrinho.nome = 'Pedrinho'
+personagemPedrinho.mana = 12
+personagemPedrinho.vida = 7
+
+console.log(personagemPedrinho);
 
