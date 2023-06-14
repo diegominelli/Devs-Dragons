3a95769dec9157463c15f80715a2a3685a32548d
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2023-06-14 17:44:23 -0300

    construindo por parametro

--- src/index.js
+++ src/index.js
@@ -1,33 +1,4 @@
 import { Personagem } from "./modules/personagem.js"
 
-// const personagemPedrinho = {
-//   nome: 'Pedrinho',
-//   vida: 7,
-//   mana: 12,
-//   level: 5,
-//   tipo: 'Mago',
-// }
-
-const personagemJose = {
-  nome: 'Jose',
-  vida: 7,
-  mana: 6,
-  level: 3,
-  tipo: 'Arqueiro',
-}
-
-const personagemAna = {
-  nome: 'Ana',
-  vida: 8,
-  mana: 10,
-  level: 3,
-  tipo: 'Arqueiro',
-}
-
-const personagemPedrinho = new Personagem()
-personagemPedrinho.nome = 'Pedrinho'
-personagemPedrinho.mana = 12
-personagemPedrinho.vida = 7
-
-console.log(personagemPedrinho);
-
+const personagemPedrinho = new Personagem('Pedrinho', 5, 'Mago')
+const personagemJose = new Personagem('Jose', 3, 'Arqueiro')
--- src/modules/personagem.js
+++ src/modules/personagem.js
@@ -5,4 +5,17 @@ export class Personagem {
   level
   tipo
   descricao
+
+  constructor(nome, level, tipo) {
+    this.nome = nome
+    this.level = level
+    this.tipo = tipo
+  }
+
+  obterInsignia() {
+    if (this.level >= 5) {
+      return `Implacavel ${this.tipo}`
+    } 
+    return `${this.tipo} iniciante`
+  }
 }
