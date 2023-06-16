4159df531912073125172f513a36fca6d77fc256
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2023-06-16 18:16:05 -0300

    usando propriedades privadas

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import { PersonagemView } from "./components/personagem-view.js"
 import { ArqueiroMago } from "./modules/arqueiro-mago.js"
 import { Arqueiro } from "./modules/arqueiro.js"
 import { Mago } from "./modules/mago.js"
-import { Personagem } from "./modules/personagem.js"
 
 const magoAntonio = new Mago('Antonio', 4, 'fogo', 4, 3)
 const magaJulia = new Mago('Julia', 8, 'gelo', 7, 10)
