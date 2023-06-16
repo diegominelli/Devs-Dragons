1b0b414aa68818d1879b26098cd182c2ae30a21a
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2023-06-16 15:19:23 -0300

    usando a composicao

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import { PersonagemView } from "./components/personagem-view.js"
+import { ArqueiroMago } from "./modules/arqueiro-mago.js"
+import { Arqueiro } from "./modules/arqueiro.js"
 import { Mago } from "./modules/mago.js"
 
 const magoAntonio = new Mago('Antonio', 4, 'fogo', 4, 3)
 const magaJulia = new Mago('Julia', 8, 'gelo', 7, 10)
+const arqueiroBruno = new Arqueiro('Bruno', 7, 9)
+const arqueiroMagoChico = new ArqueiroMago('Chico', 7, 10, 'ar', 4, 8)
 
-const personagens = [magoAntonio, magaJulia]
+const personagens = [magoAntonio, magaJulia, arqueiroBruno, arqueiroMagoChico]
 
 new PersonagemView(personagens).render()
