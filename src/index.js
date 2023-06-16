a806129e851abcef3d21b747e188d3f37513123a
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2023-06-16 16:19:43 -0300

    usando metodos estaticos

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { PersonagemView } from "./components/personagem-view.js"
 import { ArqueiroMago } from "./modules/arqueiro-mago.js"
 import { Arqueiro } from "./modules/arqueiro.js"
 import { Mago } from "./modules/mago.js"
+import { Personagem } from "./modules/personagem.js"
 
 const magoAntonio = new Mago('Antonio', 4, 'fogo', 4, 3)
 const magaJulia = new Mago('Julia', 8, 'gelo', 7, 10)
@@ -11,3 +12,5 @@ const arqueiroMagoChico = new ArqueiroMago('Chico', 7, 10, 'ar', 4, 8)
 const personagens = [magoAntonio, magaJulia, arqueiroBruno, arqueiroMagoChico]
 
 new PersonagemView(personagens).render()
+
+console.log(Personagem.verificarVencedor(arqueiroBruno, magoAntonio));
--- src/modules/personagem.js
+++ src/modules/personagem.js
@@ -15,6 +15,20 @@ export class Personagem {
     if (this.level >= 5) {
       return `Implacavel ${this.constructor.tipo}`
     } 
+
     return `${this.constructor.tipo} iniciante`
+  }
+
+  static verificarVencedor(personagem1, personagem2) {
+    if (personagem1.level === personagem2.level) {
+      return 'Empate!!!'  
+    }
+
+    if (personagem1.level > personagem2.level) {
+      return `${personagem1.constructor.tipo} ${personagem1.nome} é o vencedor!`
+    }
+
+    return `${personagem2.constructor.tipo} ${personagem2.nome} é o vencedor!`
+
   }
 }
